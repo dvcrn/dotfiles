1ce77f9bcffb0972e74c52ca82d034008e3c0691
David Mohl <me@dave.cx>
2016-02-04 10:30:54 +0900

    added install.sh for automatic installation

---
+++ install.sh
@@ -0,0 +1,72 @@
+echo "Installing all your stuff!"
+echo ""
+
+# Brew
+echo ""
+echo "---> Brew"
+brew tap Homebrew/bundle
+cd ~/.dotfiles/brew/ && brew bundle
+cd ~/.dotfiles/
+echo "---> Brew done"
+
+echo ""
+echo "---> Git"
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+echo "---> Git done"
+
+echo ""
+echo "---> Python / pip"
+pip install -r ~/.dotfiles/pip/packages.txt
+echo "---> Python / pip done"
+
+echo ""
+echo "---> NPM"
+python ~/.dotfiles/npm/install.py
+echo "---> NPM done"
+
+echo ""
+echo "---> tmux"
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+echo "---> tmux done"
+
+echo ""
+echo "---> vim"
+ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/.gvimrc ~/.gvimrc
+mkdir -p ~/.config/nvim/
+ln -s ~/.dotfiles/vim/.nvimrc ~/.config/nvim/init.vim
+echo "---> vim done"
+
+echo ""
+echo "---> spacemacs"
+ln -s ~/.dotfiles/spacemacs/.spacemacs ~/.spacemacs
+echo "---> spacemacs done"
+
+echo ""
+echo "---> proton"
+ln -s ~/.dotfiles/proton/.proton ~/.proton
+echo "---> proton done"
+
+echo ""
+echo "---> sublimious"
+ln -s ~/.dotfiles/sublimious/.sublimious ~/.sublimious
+echo "---> sublimious done"
+
+echo ""
+echo "---> kwm"
+mkdir -p ~/.kwm/
+ln -s ~/.dotfiles/kwm/kwmrc ~/.kwm/kwmrc
+echo "---> kwm done"
+
+echo ""
+echo "---> fish"
+mkdir -p ~/.config/
+ln -s ~/.dotfiles/fisherman ~/.config/fisherman
+echo "---> fish done"
+
+echo ""
+echo "---> peco"
+mkdir -p ~/.config/peco/
+ln -s ~/.dotfiles/peco/config.json ~/.config/peco/config.json
+echo "---> peco done"

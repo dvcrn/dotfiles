1327d5aa30b583d84aaf18a3386ce5ad363a6f6e
David Mohl <me@dave.cx>
2016-02-07 14:20:00 +0900

    added ideavimrc

--- install.sh
+++ install.sh
@@ -34,6 +34,7 @@ echo ""
 echo "---> vim"
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim/.gvimrc ~/.gvimrc
+ln -s ~/.dotfiles/vim/.ideavimrc ~/.ideavimrc
 mkdir -p ~/.config/nvim/
 ln -s ~/.dotfiles/vim/.nvimrc ~/.config/nvim/init.vim
 echo "---> vim done"

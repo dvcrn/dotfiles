4af8701c62e329321d73fcdd2737e248f957b79b
David Mohl <me@dave.cx>
2016-07-12 17:33:04 +0900

    feat(osx-for-david): init
    I like osx-for-hackers but I like it a bit more biased.

---
+++ osx-for-david.sh
@@ -0,0 +1,5 @@
+# Save screenshots into ~/Screenshots/ instead of desktop
+defaults write com.apple.screencapture location ~/Screenshots/
+
+# Never re-open previous windows
+defaults write -g ApplePersistence -bool no

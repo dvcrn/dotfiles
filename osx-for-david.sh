9105ffc283e1fe9ec14f7c33967898dcd0dcc462
David Mohl <me@dave.cx>
2017-11-20 17:02:58 +0900

    feat(osx-for-david): disable gatekeeper & more

--- osx-for-david.sh
+++ osx-for-david.sh
@@ -3,3 +3,14 @@ defaults write com.apple.screencapture location ~/Screenshots/
 
 # Never re-open previous windows
 defaults write -g ApplePersistence -bool no
+
+# defaults write com.apple.dock autohide-time-modifier -int 0
+defaults write com.apple.dock autohide-time-modifier -float 0.15
+
+# disable key hold for accented input
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# goodbye gatekeeper
+sudo spctl --master-disable
+
+killall Dock

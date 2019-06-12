c6afb832705c6f52b12687d782d89bd68697d22b
David Mohl <git@davemail.io>
2019-06-12 15:58:23 +0900

    emacs: Freeze current configuration
    Slowly migrating my vim setup over to emacs. Too many changes to
    individually listen.

--- emacs/init.el
+++ emacs/init.el
@@ -38,13 +38,8 @@ There are two things you can do about this warning:
 
 (use-package swiper :ensure t)
 (use-package counsel :ensure t)
-(use-package counsel-projectile :ensure t)
-
-(use-package projectile
-  :ensure t
-  :config
-  (projectile-mode 1))
-
+(use-package counsel-projectile :ensure t :config (setq projectile-project-search-path '("~/src/" "~/go/src/github.com/kouzoh/" "~/go/src/github.com/dvcrn/")))
+(use-package projectile :ensure t :config (projectile-mode 1))
 (use-package ivy
   :ensure t
   :config
@@ -54,9 +49,23 @@ There are two things you can do about this warning:
   (setq ivy-display-style 'fancy)
   (ivy-mode 1))
 
+(use-package magit
+  :ensure t
+  :config
+  (with-eval-after-load 'evil-maps
+    (define-key evil-normal-state-map " gs" 'magit-status)))
+
+(use-package diff-hl
+  :ensure t
+  :config
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  (global-diff-hl-mode))
 
 (use-package evil
   :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
 
@@ -65,11 +74,20 @@ There are two things you can do about this warning:
   :config
   (global-evil-surround-mode 1))
 
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :custom
+  (evil-collection-company-use-tng nil)
+  :config
+  (evil-collection-init))
+
 (use-package company
   :ensure t
   :config
-  (company-mode)
-  (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
+  (global-company-mode)
+  (define-key evil-insert-state-map (kbd "C-n") 'company-select-next-if-tooltip-visible-or-complete-selection)
+  (define-key company-active-map (kbd "C-n") 'company-select-next-if-tooltip-visible-or-complete-selection)
   (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort))
 
 (use-package neotree
@@ -81,7 +99,6 @@ There are two things you can do about this warning:
   (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
   (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
   (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
-  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
   (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
   (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
   (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
@@ -117,38 +134,73 @@ There are two things you can do about this warning:
 
 (use-package yasnippet-snippets :ensure t)
 
+;; yaml
+(use-package yaml-mode :ensure t)
+(use-package flycheck-yamllint
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (eval-after-load 'flycheck
+      '(add-hook 'flycheck-mode-hook 'flycheck-yamllint-setup))))
+
 ;; language server
 (use-package lsp-mode
   :ensure t
-  :commands lsp)
-
-(add-hook 'go-mode-hook #'lsp)
+  :commands lsp
+  :hook
+  (go-mode . lsp)
+  (js-mode . lsp)
+  (elixir-mode . lsp)
+  :config
+  (defvar lsp-language-id-configuration
+     '(
+      (python-mode . "python")
+      (javascript-mode . "javascript")
+      (js-mode . "javascript"))))
 
 ;; optional - provides fancier overlays
 (use-package lsp-ui
   :ensure t
   :config
-  (lsp-ui-mode)
-  (lsp-ui-doc-mode -1)
-  (setq lsp-ui-doc-enable -1))
+ ;;'(lsp-ui-doc-enable nil)
+ '(lsp-ui-sideline-enable nil))
 
 ;; if you use company-mode for completion (otherwise, complete-at-point works out of the box):
 (use-package company-lsp
   :ensure t
   :commands company-lsp)
 
+(use-package lsp-treemacs :ensure t :commands lsp-treemacs-errors-list)
+(use-package dap-mode :ensure t)
+
 ;; languages
 (use-package go-mode :ensure t
   :config
   (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (evil-define-key 'normal go-mode-map "gd" 'godef-jump))
+  (evil-define-key 'normal go-mode-map "gd" (lambda () (interactive) (evil--jumps-push) (godef-jump))))
 
-(use-package flycheck-gometalinter
+(use-package flycheck-golangci-lint
   :ensure t
+  :hook (go-mode . flycheck-golangci-lint-setup)
   :config
-  (progn
-    (flycheck-gometalinter-setup)))
+  ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+  (setq flycheck-gometalinter-vendor t)
+  ;; only show errors
+  (setq flycheck-gometalinter-errors-only t)
+  ;; only run fast linters
+  (setq flycheck-gometalinter-fast t)
+  ;; use in tests files
+  (setq flycheck-gometalinter-test t)
+  ;; disable linters
+  ;; (setq flycheck-gometalinter-disable-linters '("gotype" "gocyclo"))
+  ;; Only enable selected linters
+  ;; (setq flycheck-gometalinter-disable-all t)
+  ;; (setq flycheck-gometalinter-enable-linters '("golint"))
+  ;; Set different deadline (default: 5s)
+  (setq flycheck-gometalinter-deadline "10s"))
+
 (use-package company-go
   :ensure t
   :config
@@ -158,6 +210,24 @@ There are two things you can do about this warning:
 
 (use-package applescript-mode :ensure t)
 
+;; Elixir
+(use-package elixir-mode :ensure t)
+(use-package alchemist :ensure t :hook (elixir-mode . alchemist-mode))
+(use-package flycheck-mix :ensure t)
+
+;; Lisps
+(use-package lispy :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1))))
+
+(use-package lispyville :ensure t
+  :config
+  (add-hook 'lispy-mode-hook #'lispyville-mode)
+  (setq lispyville-key-theme '(operators c-w slurp/barf-cp prettify additional additional-insert))
+  (lispyville-set-key-theme))
+
+(use-package highlight-parentheses :ensure t :config (global-highlight-parentheses-mode))
+
 
 ;; take gopath from users shell settings
 (when (memq window-system '(mac ns))
@@ -165,20 +235,17 @@ There are two things you can do about this warning:
   (exec-path-from-shell-copy-env "GOPATH")
   (exec-path-from-shell-copy-env "PATH"))
 
-;; start emacs server
-(server-start)
-
-;; hide startup screen
-(setq inhibit-startup-screen t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode 1)
-
-;; font
+;; Options
+(server-start) 				;; start emacs server
+(setq inhibit-startup-screen t) 	;; hide startup screen
+(scroll-bar-mode -1) 			;; Don't care about scrollbar
+(tool-bar-mode -1) 			;; Don't care about toolbar
+(menu-bar-mode 1)            	        ;; Not setting this makes the window uncontrollable with magnet
+(show-paren-mode 1)	                ;; Highlight closing paren
+(electric-pair-mode) 			;; automatically insert closing brackets
+(global-hl-line-mode 1) 		;; highlight current line
 (set-frame-font "Operator Mono 15" nil t)
 
-;; highlight current line
-(global-hl-line-mode 1)
 
 ;;Exit insert mode by pressing j and then j quickly
 (setq key-chord-two-keys-delay 0.5)
@@ -192,19 +259,9 @@ There are two things you can do about this warning:
 (define-key evil-normal-state-map  " pf" 'counsel-projectile-find-file)
 (define-key evil-normal-state-map  " p/" 'counsel-projectile-ag)
 (define-key evil-normal-state-map  (kbd "C-p") 'counsel-projectile-find-file)
-
-
-;(define-key projectile-mode-map  " pf" 'counsel-projectile-find-file)
-;(define-key projectile-mode-map  " p/" 'counsel-projectile-ag)
-;(define-key projectile-mode-map  (kbd "C-p") 'counsel-projectile-find-file)
+(define-key evil-normal-state-map  " pp" 'counsel-projectile-switch-project)
 
 (define-key evil-normal-state-map  " ff" 'counsel-projectile)
-
-;; navigate with ctrl
-;;(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-;;(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-;;(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-;;(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 
 ;; window
 (define-key evil-normal-state-map  " wv" 'split-window-horizontally)
@@ -213,18 +270,21 @@ There are two things you can do about this warning:
 (define-key evil-normal-state-map  "]q" 'flycheck-next-error)
 (define-key evil-normal-state-map  "[q" 'flycheck-previous-error)
 
+(define-key evil-normal-state-map  "]c" 'diff-hl-next-hunk)
+(define-key evil-normal-state-map  "[c" 'diff-hl-previous-hunk)
+
 ;; indent when hitting REt
 (define-key evil-insert-state-map (kbd "RET") 'newline-and-indent)
 
 
 ;; switch to last tab
 (defun switch-to-last-buffer ()
+  "Switch to last active buffer."
   (interactive)
   (switch-to-buffer nil))
 
 (define-key evil-normal-state-map  (kbd "SPC TAB") 'switch-to-last-buffer)
-;; (define-key dired-mode-map  (kbd "SPC TAB") 'switch-to-last-buffer)
-
+;; (define-key dired-mode-map (kbd "SPC TAB") 'switch-to-last-buffer)
 
 ;; close ivy with esc
 (define-key ivy-minibuffer-map [escape] 'minibuffer-keyboard-quit)
@@ -233,7 +293,7 @@ There are two things you can do about this warning:
 ;; delete last word with C-w in ivy-mode
 (define-key ivy-minibuffer-map  (kbd "C-w") 'evil-delete-backward-word)
 
-	;; Keybonds
+;; Keybonds
 (global-set-key [(hyper a)] 'mark-whole-buffer)
 (global-set-key [(hyper v)] 'yank)
 (global-set-key [(hyper c)] 'kill-ring-save)
@@ -247,20 +307,25 @@ There are two things you can do about this warning:
 (setq mac-option-modifier 'meta)
 (setq mac-command-modifier 'hyper)
 
+;; Improve scrolling
 (setq scroll-step            1
       scroll-conservatively  10000)
+
 ;;---------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-collection-company-use-tng nil)
+ '(lispyville-motions-put-into-special t)
+ '(lsp-ui-sideline-enable nil)
  '(package-selected-packages
    (quote
-    (nlinum-relative yasnippet-snippets use-package-chords smart-mode-line seoul256-theme neotree lsp-ui flycheck-gometalinter exec-path-from-shell evil-surround counsel-projectile company-lsp company-go))))
+    (flycheck-yamllint yaml-mode kubernetes magit evil-collection flycheck-golangci-lint lispy lispyville highlight-parentheses evil-cleverparens dap-mode lsp-treemacs smartparens flycheck-mix nlinum-relative yasnippet-snippets use-package-chords smart-mode-line seoul256-theme neotree lsp-ui flycheck-gometalinter exec-path-from-shell evil-surround counsel-projectile company-lsp company-go))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Operator Mono" :foundry "nil" :slant normal :weight normal :height 151 :width normal)))))
+ '(default ((t (:foreground "#D9D9D9" :background "#4B4B4B")))))

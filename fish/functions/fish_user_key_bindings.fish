482901bf79334aa5f3ceeb6fccfb8461c05d5ca0
David Mohl <git@davemail.io>
2021-12-23 11:19:56 +0900

    fish: Correctly source fzf completions

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,7 @@
 function fish_user_key_bindings
-  fzf_key_bindings
+  if type -q fzf_key_bindings
+    fzf_key_bindings
+  else 
+    if [ -f '(brew --prefix)/opt/fzf/shell/key-bindings.fish']; . '(brew --prefix)/opt/fzf/shell/key-bindings.fish'; end
+  end
 end

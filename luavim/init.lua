3eb0badd1c97dfa20d87fff1b62514772699a7b5
David Mohl <git@d.sh>
2025-01-13 20:28:46 +0900

    nvim: Add auto packer.nvim bootstrap

--- luavim/init.lua
+++ luavim/init.lua
@@ -1,3 +1,26 @@
+local ensure_packer = function()
+    local fn = vim.fn
+
+    local install_dir = fn.expand("~/.local/share/nvim/site/pack/packer/start/")
+    local install_path = install_dir .. "packer.nvim"
+
+    if fn.empty(fn.glob(install_path)) > 0 then
+        print("config is empty, installing packer....")
+        print(install_path)
+
+        fn.system({ 'mkdir', '-p', install_dir })
+        fn.system({ 'git', 'clone', '--depth', '1', 'git@github.com:wbthomason/packer.nvim.git', install_path })
+
+        vim.cmd [[packadd packer.nvim]]
+
+        print("packer.nvim has been installed. please restart neovim and run :PackerInstall")
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 require "options"
 require "keybindings"
 require "style"

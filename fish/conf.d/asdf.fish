8b616bcfdff477a4d6570a750a32c7b2935643e5
David Mohl <git@d.sh>
2025-06-09 14:45:00 +0900

    Update asdf shims

--- fish/conf.d/asdf.fish
+++ fish/conf.d/asdf.fish
@@ -1,3 +1,13 @@
-if type -q asdf
-    source (brew --prefix asdf)"/libexec/asdf.fish"
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
